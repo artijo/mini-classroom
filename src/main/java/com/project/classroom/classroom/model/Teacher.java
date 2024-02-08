461855f870885f2eb871b19a2d465f0f60ceb1b8
tijo <81065469+artijo@users.noreply.github.com>
2024-02-08 14:15:02 +0700

    init

---
+++ src/main/java/com/project/classroom/classroom/model/Teacher.java
@@ -0,0 +1,70 @@
+package com.project.classroom.classroom.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Teacher {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idTeacher;
+	private String fname;
+	private String lname;
+	private String email;
+	private String password;
+	private Date createdAt;
+	private Date updatedAt;
+	private Date deletedAt;
+	public int getIdTeacher() {
+		return idTeacher;
+	}
+	public void setIdTeacher(int idTeacher) {
+		this.idTeacher = idTeacher;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public Date getDeletedAt() {
+		return deletedAt;
+	}
+	public void setDeletedAt(Date deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+}
---
+++ src/main/java/com/project/classroom/classroom/model/TeacherInterface.java
@@ -0,0 +1,7 @@
+package com.project.classroom.classroom.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeacherInterface extends CrudRepository<Teacher, Integer>{
+
+}
