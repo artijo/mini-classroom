395406f9aca66f0e5af0597baffeb545e0a63a74
Kuy gu dum dev <125670874+PeeraponWWW@users.noreply.github.com>
2024-02-28 14:03:27 +0700

    pysh

--- src/main/java/com/project/classroom/classroom/uploadservice/UploadService.java
+++ src/main/java/com/project/classroom/classroom/uploadservice/UploadService.java
@@ -4,23 +4,21 @@ import java.io.File;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.multipart.MultipartFile;
 
 
 public class UploadService {
 
-//	private  String pathStatic
-	
-	@Value("${pathStatic}")
     private String path = "E:" + File.separator + "Github" + File.separator + "classroom" + File.separator +
     "src" + File.separator + "main" + File.separator + "resources" + File.separator + "static" + File.separator + "file";
 
+    
     public String upload(MultipartFile file) {
         try {
             String originalFilename = file.getOriginalFilename();
             String uniqueFileName = System.currentTimeMillis() + "&" + originalFilename;
             File directory = new File(path);
-            
             if (!directory.exists()) {
                 directory.mkdirs();
             }
